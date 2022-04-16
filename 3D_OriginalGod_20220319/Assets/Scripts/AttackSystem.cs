45c60b950d783cc5ce22fde5cfea701bf0a40765
KID <50666207+KID421@users.noreply.github.com>
2022-04-16 17:00:42 +0800

    Update 收刀

--- 3D_OriginalGod_20220319/Assets/Scripts/AttackSystem.cs
+++ 3D_OriginalGod_20220319/Assets/Scripts/AttackSystem.cs
@@ -25,8 +25,16 @@ namespace KID
         private GameObject goWeaponHand;
         [SerializeField, Header("刀光")]
         private ParticleSystem psLight;
+        [SerializeField, Header("第一階段收刀時間")]
+        private float timeSwordToBack = 2.5f;
+        [SerializeField, Header("第二階段收刀時間")]
+        private float timeSwordToHide = 3.5f;
 
         private string parameterAttack = "觸發攻擊";
+        private bool isAttack;
+        private bool isBack;
+        private float timer;
+        private float timerToHide;
         #endregion
 
         #region 事件
@@ -38,6 +46,8 @@ namespace KID
         private void Update()
         {
             SwitchWeapon();
+            SwordToBack();
+            SwordToHide();
         }
         #endregion
 
@@ -54,6 +64,50 @@ namespace KID
 
                 ani.SetTrigger(parameterAttack);    // 觸發攻擊動畫
                 psLight.Play();                     // 播放攻擊特效
+
+                timer = 0;                          // 每次攻擊計時器重算
+                isAttack = true;
+            }
+        }
+
+        /// <summary>
+        /// 收刀第一階段：刀子從手上收回背後
+        /// </summary>
+        private void SwordToBack()
+        {
+            if (isAttack)
+            {
+                timer += Time.deltaTime;
+
+                if (timer >= timeSwordToBack)
+                {
+                    goWeaponHand.SetActive(false);
+                    goWeaponBack.SetActive(true);
+
+                    timer = 0;                      // 收刀後計時器歸零
+                    timerToHide = 0;
+                    isAttack = false;
+                    isBack = true;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 收刀第二階段：從背後隱藏
+        /// </summary>
+        private void SwordToHide()
+        {
+            if (isBack)
+            {
+                timerToHide += Time.deltaTime;
+
+                if (timerToHide >= timeSwordToHide)
+                {
+                    goWeaponBack.SetActive(false);
+
+                    timerToHide = 0;
+                    isBack = false;
+                }
             }
         }
         #endregion
